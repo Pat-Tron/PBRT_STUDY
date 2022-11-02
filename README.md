c6a210dfd0b057166cad6c3b1e4188db052731ce
Pat-Tron <perpendiculaa@gmail.com>
2022-11-03 00:32:12 +0800

    weekend finished

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # pbrt
 
+### weekendFinish
+![weekendFinish](milestones/weekendFinish.png "weekendFinish")
+### glassIOR115
+![glassIOR115](milestones/glassIOR115.png "glassIOR115")
+### 3materials
+![3materials](milestones/3materials.png "3materials")
 ### depth5antialiasing100
 ![depth5antialiasing100](milestones/depth5antialiasing100.png "depth5antialiasing100")
-![3materials](milestones/3materials.png "3materials")
-![glassIOR115](milestones/glassIOR115.png "glassIOR115")
+
+
--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 #include "Shape.h"
 
 int main() {
-    Camera camera(P1K, 0.5);
+    Camera camera(P1K);
     camera.position = Vec3(20, 4.5, -2);
     camera.focal = 3.0 ;
-    camera.antialiasing = 1 ;
+    camera.antialiasing = 10 ;
     camera.maxDepth = 10;
     camera.aperture = 0.2;
     camera.defocusScale = 0.9;
@@ -16,9 +16,13 @@ int main() {
 
     Lambertian diffuseGround(0x5654f7);
     Lambertian diffuseYellow(0xffe815);
+    Lambertian diffusePurple(0x64499a);
     Metal shinyRed(0xF72349);
+    Metal shinyOrange(0xff5c29);
     Metal matteGreen(0x50BF94, 0.1);
+    Metal matteGreen2(0xadce33, 0.2);
     Dielectric glass(0xffffFF, 1.4); // 0x2222DD
+    Dielectric glass2(0xddddFF, 1.3); // 0x2222DD
 
     double squareSize{ 7.0 };
     Vec3 corners[4]{
@@ -34,7 +38,14 @@ int main() {
         std::make_shared<Sphere>(Sphere(1.0,  Vec3(0.0, 1, 0.0), &shinyRed)),  // m small
         std::make_shared<Sphere>(Sphere(1.0,  Vec3(1.5, 1, -1.5), &matteGreen)),  // r small
         std::make_shared<Sphere>(Sphere(0.7,  Vec3(2, 0.7, 0.8), &glass)),  // front small
+
+        std::make_shared<Sphere>(Sphere(0.2,  Vec3(2.5, 0.2, -1.8), &diffusePurple)),
+        std::make_shared<Sphere>(Sphere(0.3,  Vec3(2, 0.3, -0.5), &glass2)),
+        std::make_shared<Sphere>(Sphere(0.4,  Vec3(-3, 0.4, 3), &matteGreen2)),
+        std::make_shared<Sphere>(Sphere(0.4,  Vec3(0.8, 0.4, -2.7), &shinyOrange))
     } };
+
+    // TODO: random balls
 
     camera.randerLoop(primitives);
 
